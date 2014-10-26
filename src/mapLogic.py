803077f735d14c5669442a6a1c1216270659532f
dre576 <aj90008@gmail.com>
2014-10-25 19:01:12 -0600

    unit test suite
    made a unit test suite that we can use, implemented one unit test to
    make sure teleport works, also added return True to doMapLogic so it
    could be easily tested

--- src/mapLogic.py
+++ src/mapLogic.py
@@ -7,4 +7,5 @@ Created on 20 Oct 2014
 def doMapLogic(scenemap, x, y, facing): 
     if scenemap == "town1" and x >= 19 and x <= 21 and y >= 29 and facing == "down":
         print("Teleport")
+        return True
         
