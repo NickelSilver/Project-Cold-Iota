7d7f6f5812de5da2211ef8de6fb25fb299620ede
Matthew Dolan <mdolan2424@gmail.com>
2014-10-27 23:12:51 -0600

    Unit Tests for NPC response.

--- src/npcs/npcTests.py
+++ src/npcs/npcTests.py
@@ -4,23 +4,28 @@ Created on Oct 25, 2014
 @author: Matt
 '''
 import unittest
-from npcs import npc
+from npc import npc
 
 class Test(unittest.TestCase):
     
-
     def testName(self):
         pass
     
-    def speak(self, text):
-        print(text)
+    def testResponse(self):
         
-    def speakTo(self, text, destination):
-        print(text)
-    
-    def move(self):
-        ""
+        npc1 = npc("town1", 7, 7, "down", "sprites/npc1SpriteSheet1.png")
+        npc2 = npc("town1", 7, 7, "down", "sprites/emoSpriteSheet.png")
+        npc3 = npc("town1", 7, 7, "down", "sprites/goatBoySpriteSheet1.png")
+        npc4 = npc("town1", 7, 7, "down", "sprites/holstaurusSpriteSheet.png")
+        npc5 = npc("town1", 7, 7, "down", "sprites/horsinAroundSpriteSheet1.png")
+        npc6 = npc("town1", 7, 7, "down", "sprites/monkeyBusinessSpriteSheet1.png")
+        npc7 = npc("town1", 7, 7, "down", "sprites/mrPiddlesSpriteSheet1.png")
         
+        print(npc3.getResponse())
+        print(npc3.getResponse())
+        print(npc3.getResponse())
+        
+        pass
     
     
 
