80cd3950e569b2fe27ffd920949b2e55889e6943
dre576 <aj90008@gmail.com>
2014-11-02 12:58:52 -0700

    unit tests
    combined npc tests with other unit tests into class test suite

--- src/testSuite.py
+++ src/testSuite.py
@@ -1,13 +1,34 @@
 __author__ = 'Andrea'
 import unittest
-
+from npcs.npc import *
 from mapLogic import *
+
 
 class tests(unittest.TestCase):
 
-
+    #-----------------maplogictests-------------------------
     def test_doMapLogic(self):
         self.assertTrue(doMapLogic("town1", 20, 30, "down"))
+
+
+
+    #-------------NPC tests-------------------------
+
+    def testResponse(self):
+
+        npc1 = npc("town1", 7, 7, "down", "sprites/npc1SpriteSheet1.png")
+        npc2 = npc("town1", 7, 7, "down", "sprites/emoSpriteSheet.png")
+        npc3 = npc("town1", 7, 7, "down", "sprites/goatBoySpriteSheet1.png")
+        npc4 = npc("town1", 7, 7, "down", "sprites/holstaurusSpriteSheet.png")
+        npc5 = npc("town1", 7, 7, "down", "sprites/horsinAroundSpriteSheet1.png")
+        npc6 = npc("town1", 7, 7, "down", "sprites/monkeyBusinessSpriteSheet1.png")
+        npc7 = npc("town1", 7, 7, "down", "sprites/mrPiddlesSpriteSheet1.png")
+
+        print(npc3.getResponse())
+        print(npc3.getResponse())
+        print(npc3.getResponse())
+
+        pass
 
 
 
