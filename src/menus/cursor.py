da0c008c514494e4a70d665e2855f6fbfc7d4e27
dre576 <aj90008@gmail.com>
2014-11-01 18:02:22 -0600

    cursor class
    create a cursor to track movement in menus and help display movement to
    user

---
+++ src/menus/cursor.py
@@ -0,0 +1,38 @@
+__author__ = 'Andrea'
+
+class Cursor:
+    def __init__(self, xpos=0, ypos=0, size=10, jump=5, image = None):
+        self.x = xpos
+        self.y = ypos
+        self.size = size
+        self.jump = jump
+        self.count = 0
+        self.image = image
+
+    def moveUp(self):
+        self.count-=1
+        if self.count>=0:
+            self.y +=self.jump
+
+    def moveDown(self):
+        self.y-=self.jump
+        self.count+=1
+
+    def setPos(self, xpos, ypos):
+        self.x = xpos
+        self.y = ypos
+
+    def getPos(self):
+        return (self.x,self.y)
+
+    def setJump(self, jump):
+        self.jump = jump
+
+    def getJump(self):
+        return self.jump
+
+    def setImage(self, image):
+        self.image = image
+
+    def getImgae(self):
+        return self.image
