ae02f693005c95c44dbb00e7c0fedfc710f09ede
dre576 <aj90008@gmail.com>
2014-11-14 12:43:08 -0700

    hero class
    basic hero class implemented

---
+++ src/combat/hero.py
@@ -0,0 +1,24 @@
+__author__ = 'Andrea'
+
+class hero:
+    health = 100
+    speed = 10
+    mana = 100
+    attack = 5
+    defense = 3
+    level = 1
+    experience = 0
+    XPneeded = 100
+
+    def fight(self):
+        pass
+
+    def defend(self):
+        pass
+
+    def useMagic(self):
+        pass
+
+    def levelUp(self):
+        pass
+
